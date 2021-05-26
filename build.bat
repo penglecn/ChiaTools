1d69b07a6dc0e6f0c3cd4a1c096712dc5e423bd0
devteamcn <pengbeicn@me.com>
2021-05-26 15:34:59 +0800

    解决部分启动报错

--- build.bat
+++ build.bat
@@ -4,3 +4,10 @@ rd /s /q dist
 rem pyinstaller main.pyw -D -c --manifest ChiaTools.exe.manifest --uac-admin
 pyinstaller -F main.spec
 copy C:\Python37-64\Lib\site-packages\mpir_skylake_avx.dll .\dist\ChiaTools\
+copy C:\Python37-64\Lib\site-packages\mpir_broadwell.dll .\dist\ChiaTools\
+copy C:\Python37-64\Lib\site-packages\mpir_broadwell_avx.dll .\dist\ChiaTools\
+copy C:\Python37-64\Lib\site-packages\mpir_bulldozer.dll .\dist\ChiaTools\
+copy C:\Python37-64\Lib\site-packages\mpir_gc.dll .\dist\ChiaTools\
+copy C:\Python37-64\Lib\site-packages\mpir_haswell.dll .\dist\ChiaTools\
+copy C:\Python37-64\Lib\site-packages\mpir_piledriver.dll .\dist\ChiaTools\
+copy C:\Python37-64\Lib\site-packages\mpir_sandybridge.dll .\dist\ChiaTools\
