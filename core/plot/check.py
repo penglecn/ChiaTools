cbce195d851fb25f9a7b521a4438783783477162
devteamcn <pengbeicn@me.com>
2021-08-07 21:12:08 +0800

    优化plot检查功能

--- core/plot/check.py
+++ core/plot/check.py
@@ -1,9 +1,105 @@
 from PyQt5.Qt import pyqtSignal
 from PyQt5.Qt import QThread
 from subprocess import Popen, PIPE, STDOUT, CREATE_NO_WINDOW
-import os
 import re
 from core import is_debug, BASE_DIR
+from time import time
+import os
+from chiapos import Verifier, DiskProver
+from utils.chia.hash import std_hash
+from utils.chia.plotting.plot_tools import parse_plot_info
+
+
+def get_plot_info(plot_path):
+    try:
+        prover = DiskProver(plot_path)
+        (
+            is_pool_contract_puzzle_hash,
+            pool_public_key_or_puzzle_hash,
+            farmer_public_key,
+            local_master_sk,
+        ) = parse_plot_info(prover.get_memo())
+
+        pool_public_key = ''
+        pool_contract_puzzle_hash = ''
+
+        if is_pool_contract_puzzle_hash:
+            pool_contract_puzzle_hash = pool_public_key_or_puzzle_hash
+        else:
+            pool_public_key = pool_public_key_or_puzzle_hash
+
+        return {
+            'path': plot_path,
+            'prover': prover,
+            'farmer_public_key': farmer_public_key,
+            'pool_public_key': pool_public_key,
+            'pool_contract_puzzle_hash': pool_contract_puzzle_hash,
+            'local_master_sk': local_master_sk,
+            'plot_size': os.path.getsize(plot_path),
+        }
+    except:
+        return None
+
+
+def check_plot_quality(plot, challenges=30):
+    if isinstance(plot, str):
+        plot = get_plot_info(plot)
+        if not plot:
+            return False, 0
+
+    v = Verifier()
+
+    prover = plot['prover']
+
+    total_proofs = 0
+    caught_exception: bool = False
+    for i in range(challenges):
+        challenge = std_hash(i.to_bytes(32, "big"))
+        try:
+            # quality_start_time = int(round(time() * 1000))
+            for index, quality_str in enumerate(prover.get_qualities_for_challenge(challenge)):
+                # quality_spent_time = int(round(time() * 1000)) - quality_start_time
+                # if quality_spent_time > 5000:
+                #     log.warning(
+                #         f"\tLooking up qualities took: {quality_spent_time} ms. This should be below 5 seconds "
+                #         f"to minimize risk of losing rewards."
+                #     )
+                # else:
+                #     log.info(f"\tLooking up qualities took: {quality_spent_time} ms.")
+
+                # Other plot errors cause get_full_proof or validate_proof to throw an AssertionError
+                try:
+                    # proof_start_time = int(round(time() * 1000))
+                    proof = prover.get_full_proof(challenge, index, True)
+                    # proof_spent_time = int(round(time() * 1000)) - proof_start_time
+                    # if proof_spent_time > 15000:
+                    #     log.warning(
+                    #         f"\tFinding proof took: {proof_spent_time} ms. This should be below 15 seconds "
+                    #         f"to minimize risk of losing rewards."
+                    #     )
+                    # else:
+                    #     log.info(f"\tFinding proof took: {proof_spent_time} ms")
+                    total_proofs += 1
+                    ver_quality_str = v.validate_proof(prover.get_id(), prover.get_size(), challenge, proof)
+                    assert quality_str == ver_quality_str
+                except AssertionError as e:
+                    # log.error(f"{type(e)}: {e} error in proving/verifying for plot {path}")
+                    caught_exception = True
+                quality_start_time = int(round(time() * 1000))
+        except KeyboardInterrupt:
+            # log.warning("Interrupted, closing")
+            return False, 0
+        except SystemExit:
+            # log.warning("System is shutting down.")
+            return False, 0
+        except Exception as e:
+            # log.error(f"{type(e)}: {e} error in getting challenge qualities for plot {path}")
+            caught_exception = True
+        if caught_exception is True:
+            break
+    if total_proofs > 0 and caught_exception is False:
+        return True, round(total_proofs/float(challenges), 4)
+    return False, round(total_proofs/float(challenges), 4)
 
 
 class PlotInfo(object):
